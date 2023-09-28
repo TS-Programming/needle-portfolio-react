d0b30301af70e80281de08053e8c1c015e49a8f5
Tanner Samples <83928312+TS-Programming@users.noreply.github.com>
2023-09-28 08:10:01 -0700

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 
-import React, { useRef } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home/home';
+import Pentago from './GamePages/pentago';
 
 
 function App() {
  
   return (
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pentago" element={<Pentago />} />
+      </Routes>
+    </Router>
   );
 }
 
