c53cd9fa5c30cd6a09fbb251c7ed253ec305f126
Tanner Samples <83928312+TS-Programming@users.noreply.github.com>
2024-01-02 03:15:28 -0800

    updated world ui button urls

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,14 +2,14 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/floatingChest.glb?v=1704193551411");
+needle_exported_files.push("assets/floatingChest.glb?v=1704194059025");
 
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1704193551411");
+		needleEngine.setAttribute("hash", "1704194059025");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
