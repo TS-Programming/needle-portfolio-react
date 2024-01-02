8a8e1a56c92bcf17ece14e07e7a0ba32a076d6ca
Tanner Samples <83928312+TS-Programming@users.noreply.github.com>
2024-01-02 03:55:51 -0800

    Changed name of needle scene

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,14 +2,14 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/floatingChest.glb?v=1704196247141");
+needle_exported_files.push("assets/tannerSamples.glb?v=1704196508035");
 
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1704196247141");
+		needleEngine.setAttribute("hash", "1704196508035");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
