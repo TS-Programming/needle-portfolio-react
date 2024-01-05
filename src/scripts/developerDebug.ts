74fd454e5e914726fc89fd21833fe48b417963fe
Tanner Samples <83928312+TS-Programming@users.noreply.github.com>
2024-01-04 22:39:54 -0800

    disabled island meshes

---
+++ src/scripts/developerDebug.ts
@@ -0,0 +1,9 @@
+import {Behaviour} from "@needle-tools/engine";
+
+
+export class DeveloperDebug extends Behaviour {
+  
+    start() {
+       console.log("Testing disabled meshes on islands");
+    }
+}
